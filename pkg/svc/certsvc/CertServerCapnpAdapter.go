c135c621c08360c3c60b9bbf06f6195d40467126
Henk <henk@spaaij.net>
2022-08-27 10:06:10 -0700

    Commit prior to moving from wostzone to iotmesh

---
+++ pkg/svc/certsvc/CertServerCapnpAdapter.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"io"
+	"time"
+
+	"capnproto.org/go/capnp/v3/rpc"
+
+	"github.com/wostzone/hub/pkg/svc/certsvc/selfsigned"
+	"github.com/wostzone/hub/pkg/svc/certsvc/service"
+	"github.com/wostzone/wost.rpc/go/capnp/svc"
+)
+
+// CertServerCapnpAdapter implements the capnproto generated interface CertService_Server
+type CertServerCapnpAdapter struct {
+	srv service.ICertService
+}
+
+func (csca *CertServerCapnpAdapter) CreateClientCert(context.Context, svc.CertService_createClientCert) error {
+	return nil
+}
+
+func (csca *CertServerCapnpAdapter) CreateDeviceCert(context.Context, svc.CertService_createDeviceCert) error {
+	return nil
+}
+func (csca *CertServerCapnpAdapter) CreateServiceCert(context.Context, svc.CertService_createServiceCert) error {
+	return nil
+}
+
+//
+func ServeCertService(srv *selfsigned.SelfSignedCertService, rwc io.ReadWriteCloser) error {
+	main := svc.CertService_ServerToClient(&CertServerCapnpAdapter{
+		srv: srv,
+	})
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*3)
+	// Listen for calls, using the logger as the bootstrap interface.
+	conn := rpc.NewConn(rpc.NewStreamTransport(rwc), &rpc.Options{
+		BootstrapClient: main.Client,
+	})
+	defer conn.Close()
+
+	// Wait for connection to abort.
+	select {
+	case <-conn.Done():
+		return nil
+	case <-ctx.Done():
+		return conn.Close()
+	}
+}
